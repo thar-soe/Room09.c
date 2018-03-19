d7df43cb438983054a477bd011a6d79d46eb1890
Thar Soe <tsoe1@nsmb208-xx.csudh.edu>
2018-03-19 16:26:50 -0700

    Version 01

---
+++ room09.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void main (void)
+{
+	int x, y, z, i, h, g, k, choice; 
+	char name[256];
+
+
+	printf("Please enter your name: ");
+	scanf("%s", name); 
+}
