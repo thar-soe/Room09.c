c4f6b2addfcf0cad22ceb988938dff64d3934a57
Thar Soe <tsoe1@nsmb208-xx.csudh.edu>
2018-03-19 16:45:10 -0700

    this is version 02

--- room09.c
+++ room09.c
@@ -1,12 +1,52 @@
-#include <stdlib.h>
+nclude <stdlib.h>
 #include <stdio.h>
 
 void main (void)
 {
-	int x, y, z, i, h, g, k, choice; 
-	char name[256];
+	        int x, y, z, i, h, g, k, choice; 
+		 char name[256];
+		
+		
+		 printf("Please enter your name: ");
+		 scanf("%s", name); 
+
+		printf("Hello, %s. You only have one choice of the room \n", name);
+		printf("What do you want to do? \n"); 
+		printf("Open the door, or stay safe and NOT open it \n" ); 
+		printf("Type 'O' to open and Type 's' to stay \n"); 
+		scanf("%c", choice); 
 
 
-	printf("Please enter your name: ");
-	scanf("%s", name); 
+		if (choice == 'O'|| choice == 'o')
+		{
+			printf("You have fallen into a vat of acid. :( "); 
+			
+
+
+
+		}
+
+		else if (choice == 's' || choice == 'S')
+		{
+			printf("Oh good, you're completely safe"); 
+
+
+
+		}
+
+
+
 }
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+-- INSERT --                                                  12,2          All
+
